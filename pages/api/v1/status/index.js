15f29d0f27acb649c2e087f2ebdddd1ecf94cb4f
jsvdevcode <jsvdevcode@gmail.com>
2024-12-13 22:37:43 +0000

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,33 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "Ok" });
+  const updatedAt = new Date().toISOString();
+
+  const resultQueryServerVersion = await database.query("SHOW server_version;");
+  const serverVersion = resultQueryServerVersion.rows[0].server_version;
+
+  const resultQueryMaxConnections = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = parseInt(
+    resultQueryMaxConnections.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const resultQueryOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = resultQueryOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersion,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBe(
+    new Date(responseBody.updated_at).toISOString(),
+  );
+  expect(responseBody.dependencies?.database).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBe("16.6");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
